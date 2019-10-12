bf4bee77e5b15af4efe813c2c1e40723dbe2f5c7
lpavlo <30903282+lpavlo@users.noreply.github.com>
2019-10-12 04:46:06 -0400

    Update fourierTransform.py

--- fourierTransform.py
+++ fourierTransform.py
@@ -2,32 +2,48 @@ import matplotlib.pyplot as plt
 from scipy.fftpack import fft
 from scipy.io import wavfile # get the api
 import numpy as np
+import math
 
-def get_note(data):
-    c = fft(data)
+def get_note(rawdata): #take fourier transform
+    c = fft(rawdata)
     note = (abs(c).tolist().index(max(abs(c).tolist())))/3
+    note = round(note, 1)
     return note
 
-def seperate_notes(whole_recording, threshold):
-    seperated_notes=[]
-    j = 0
-    for i in range(int(len(data)/10)-100):
-        if sum(abs(whole_recording[i*10:i*10+100])) < threshold:
-            single_note = whole_recording[j*10:i*10]
-            seperated_notes.append(single_note)
-            j = i
-    single_note = whole_recording[j*10:i*10]
-    seperated_notes.append(single_note)
-    return(seperated_notes)
 
-def get_time(seperated_note, fs):
+def get_time(seperated_note, fs): #determines how long note was
     time_in_sec = len(seperated_note)/fs
+    time_in_sec = round(time_in_sec, 3)
     return(time_in_sec)
 
-fs, data = wavfile.read(filename) # load the data
-notes = seperate_notes(data, 10)
-print(len(notes))
-freq_and_time = []
-for k in range(len(notes)):
-    freq_and_time.append([get_note(notes[k]), get_time(notes[k],fs)])
-print(freq_and_time)
+def seperate_notes(whole_recording, threshold, fs): 
+    seperated_notes=[]
+    j = 0
+    count = 0
+    for i in range(int(len(data)/441)):
+        #print(str(len(whole_recording)) + ' ' + str(i*10 +100))
+        if abs(whole_recording[i*441]) < threshold:
+            count = count + 1
+        else:
+            if count > 10:
+                # parce previous note and adds to freq/time array
+                #print(j)
+                #print((i+math.floor(count/2))*10)
+                single_note = whole_recording[j:(i+math.floor(count/2))*441]
+                freq_and_time.append([get_note(single_note), get_time(single_note,fs)])
+                j = int((i - count/2)*441)
+            count = 0
+
+    single_note = whole_recording[j:(i+math.floor(count/2))*441]
+    freq_and_time.append([get_note(single_note), get_time(single_note,fs)])
+    j = (i - count/2)*441
+    return(freq_and_time)
+
+def(wavefile):
+    fs, data = wavfile.read('delme_rec_unlimited_1k6cq55p.wav') # load the data
+    freq_and_time = []
+    notes = seperate_notes(data, sum(abs(data))/len(data), fs)
+    print(sum(abs(data))/len(data))
+    print(freq_and_time)
+    print(len(freq_and_time))
+    return(freq_and_time)
